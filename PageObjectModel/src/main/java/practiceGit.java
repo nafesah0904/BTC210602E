0d2d592a9ef1890ae0a27e8502ce154e6bb2a0f8
Your Name <nafesah0904@gmail.com>
2021-12-08 19:12:31 -0500

    comment

---
+++ PageObjectModel/src/main/java/practiceGit.java
@@ -0,0 +1,18 @@
+
+public class practiceGit {
+
+	public void reverseString() {
+	
+			String r = "tell me in reverse Nafesah";
+			r.split(" ");
+		
+		 
+			for(int i=r.length()-1; i>=0;i--) {
+				
+				System.out.println(r.charAt(i));
+			}
+			
+		
+	}
+
+}
